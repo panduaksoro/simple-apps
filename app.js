10c833de9bea13f5fabaca725455f946caa3fdb5
pandu <pandu@pelindo.co.id>
2026-05-19 14:21:57 +0700

    fixing

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const mysql = require('mysql');
 const app = express()
-const path = require('path')
+const path = require('node:path')
+app.disable("x-powered-by");
 require('dotenv').config();
 
 // Import Middleware
